ef928b64785620b82ff6aa770a878b93ba4d9cfd
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-02-22 07:40:00 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(){
+    assignment("Fridah")
+    remainder(5, 3)
+    addition(3, 4, 6, 9)
+    fact("reading")
+    }
+fun assignment(name: String){
+
+    var result = ("Hello Fridah")
+    println(result)
+}
+
+fun remainder(c:Int, d:Int){
+    var answer = c % d
+    println(answer)
+}
+
+fun addition(a: Int, b: Int, c: Int, d: Int){
+    var sum = a + b + c + d
+    println(sum)
+}
+fun fact(love: String){
+    var love = ("reading")
+    var solution = love
+    println(solution)
+}
